18a18fb6fc22d505cbc2d66828c168732bd7f731
amitclassic <76011479+amitclassic@users.noreply.github.com>
2022-05-29 21:20:48 -0600

    Add files via upload

---
+++ Student_Template_C82.js
@@ -0,0 +1,22 @@
+console.log("My first console project");
+
+            var first_name = "ANaa";
+            var last_name = "BANANaa";
+
+            var full_name =  first_name.concat(" ", " amit",last_name);
+            console.log(full_name);
+
+            var a = 1672;
+            var b = 8327;
+
+            var add = a + b;
+            console.log(add); 
+
+            var sub = a - b;
+            console.log(sub);
+
+            var mul = a * b;
+            console.log(mul);
+
+            var divide = a / b ;
+            console.log(divide);
